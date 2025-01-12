07e1b1e002f1dc311bca36323d1056a638e16cae
MMDRZA <info@mmdrza.com>
2025-01-12 22:09:26 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import os
+
+def read_file(_file):
+    current = os.path.abspath(os.path.dirname(__file__))
+    _github_path = os.path.join(current, '.github', _file)
+    with open(_github_path) as f:
+        _data = f.read()
+    return _data
+
+
+setup(
+    name='real-useragenter',
+    version="1.0.3",
+    description='Get Real user agent from auto update',
+    long_description=read_file('README.md'),
+    long_description_content_type='text/markdown',
+    url='https://github.com/useragenter/real-useragent',
+    author='Mohammadreza (MMDRZA)',
+    author_email='Pymmdrza@gmail',
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    keywords=['user-agent', 'useragenter', 'useragent', 'user agent', 'useragent', 'real user agent', 'real-useragent'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    project_urls={
+        'Bug Reports': 'https://github.com/useragenter/real-useragent/issues',
+        'Source': 'https://github.com/useragenter/real-useragent',
+    },
+)
